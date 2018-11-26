d32fd6eef2d9ff556d20d20e7227601950d0b691
HOJO3 <joe.ho@oocl.com>
2018-11-26 15:14:10 +0800

    change function name

--- src/main/java/com/tws/refactoring/Police.java
+++ src/main/java/com/tws/refactoring/Police.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Police {
-    public boolean checkDriver(Driver driver) {
+    public boolean isDriverAdult(Driver driver) {
         int adultAgeThreshold = 18;
         if(driver.getAge() >= adultAgeThreshold) return true;
         else return false;
@@ -17,7 +17,7 @@ public class Police {
     @Test
     void should_park_a_car_to_a_parking_lot_and_get_it_back() {
 
-        assertTrue(new Police().checkDriver(new Driver(18)));
-        assertFalse(new Police().checkDriver(new Driver(17)));
+        assertTrue(new Police().isDriverAdult(new Driver(18)));
+        assertFalse(new Police().isDriverAdult(new Driver(17)));
     }
 }
