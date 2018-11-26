39f713882dc2e2ab027ea9980fd7061eecf57271
HOJO3 <joe.ho@oocl.com>
2018-11-26 15:29:57 +0800

    Add a variable adultAgeThreshold to denote 18

--- src/test/java/com/tws/refactoring/PoliceTest.java
+++ src/test/java/com/tws/refactoring/PoliceTest.java
@@ -5,14 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class Police {
-    private final int adultAgeThreshold = 18;
-
-    public boolean isDriverAdult(Driver driver) {
-
-        return (driver.getAge() >= adultAgeThreshold);
-    }
-
+public class PoliceTest {
 
     @Test
     void should_park_a_car_to_a_parking_lot_and_get_it_back() {
