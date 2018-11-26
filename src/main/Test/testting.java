efa57ab0e5c62faaa411bc336c610ea23a128613
HOJO3 <joe.ho@oocl.com>
2018-11-26 15:09:51 +0800

    add test cases

---
+++ src/main/Test/testting.java
@@ -0,0 +1,10 @@
+
+package com.oocl.cultivation.test;
+
+class ParkingBoyFacts {
+    @Test
+    void should_park_a_car_to_a_parking_lot_and_get_it_back() {
+
+        assertTrue(true);
+    }
+}
--- src/main/java/com/tws/refactoring/Police.java
+++ src/main/java/com/tws/refactoring/Police.java
@@ -1,8 +1,22 @@
 package com.tws.refactoring;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class Police {
     public boolean checkDriver(Driver driver) {
         if(driver.age >= 18) return true;
         else return false;
+    }
+
+
+    @Test
+    void should_park_a_car_to_a_parking_lot_and_get_it_back() {
+
+        assertTrue(new Police().checkDriver(new Driver(18)));
+        assertFalse(new Police().checkDriver(new Driver(17)));
     }
 }
